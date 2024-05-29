d1cc3aa782d93b2100b505a9d360f7899d832e2f
AstracodeLAB <astracodelab@gmail.com>
2024-05-29 12:47:26 +0200

    Create assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+import AboutUs from "./components/AboutUs";
+
 function App() {
   return (
     <>
-      <div className="bg-primary min-h-screen">
-        <h1 className="text-3xl font-bold text-secondary">
-          Bienvenidas a Astracode<span className="text-accent">LAB</span>
-        </h1>
-      </div>
+      <AboutUs />
     </>
   );
 }
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const AboutUs = () => {
+  return (
+    <>
+      <h2>Sobre AstracodeLAB</h2>
+      <img src="" alt="" />
+    </>
+  );
+};
+
+export default AboutUs;
