920daf4581e83a53c53526d132c621e463aa04d0
AstracodeLAB <astracodelab@gmail.com>
2024-05-29 12:23:06 +0200

    Create components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 function App() {
   return (
     <>
-      <div className="bg-primary min-h-screen">
-        <h1 className="text-3xl font-bold text-secondary">
-          Bienvenidas a Astracode<span className="text-accent">LAB</span>
-        </h1>
-      </div>
+      
     </>
   );
 }
