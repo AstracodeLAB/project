978930a3008da48048e4fc3062c65da2a54ea437
Lourdesjupo <lourdes.jupo@gmail.com>
2024-06-04 13:19:39 +0200

    Change ClassName

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,16 @@ function Footer () {
 
   return(
   <> 
-    <footer class="text-white text-center flex flex-col items-center leading-loose lg:mt-24 lg:flex-row lg:justify-around lg:items-end" >
-      <div class="order-1 lg:text-left">
+    <footer className="text-white text-center flex flex-col items-center leading-loose lg:mt-24 lg:flex-row lg:justify-around lg:items-end" >
+      <div className="order-1 lg:text-left">
         <p>Aviso legal</p>
         <p>Politica de privacidad</p>
         <p>Politica de cookies</p>
       </div>
-      <div class="order-3 pt-5 lg:order-2">
+      <div className="order-3 pt-5 lg:order-2">
         <span>© 2024 | AstrocodeLAB</span>
         </div>
-      <div class="order-2 lg:order-3 lg:text-right">
+      <div className="order-2 lg:order-3 lg:text-right">
         <p>Sobre AstrocodeLAB</p>
         <p>Correo</p>
         <p>Linkedin</p>
