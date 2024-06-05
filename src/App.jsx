47486f8d9051a6d686fb086b7855922c58a5c290
Aidablaya <you@example.com>
2024-06-05 15:22:42 +0200

    correction App

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import AboutUs from './components/AboutUs';
 
 function App() {
   return (
+    <>
     <div className="min-h-screen bg-cover bg-center flex flex-col"
     style={{ backgroundImage: `url(${imgBackground})` }} >
       <Header />
       <Hero />
-      <AboutUs />
     </div>
+    <AboutUs />
+    </>
   );
 }
 
