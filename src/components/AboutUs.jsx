f8caa0b706a1f738f65cea0cfbcd21f927607352
AstracodeLAB <astracodelab@gmail.com>
2024-06-05 16:26:14 +0200

    Fix title bg gradient

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,21 +1,22 @@
-//import React from "react";
+import React from "react";
 import about from "../assets/about.png";
 
 export const AboutUs = () => {
   return (
     <>
-      <section id="about" className="mx-30 my-15">
-        <div className="relative top-10 z-0 h-4 bg-gradient-to-r from-accent via-lime-200 to-white md:h-7"></div>
-        <h2 className="relative z-10 my-4 font-sans font-medium text-2xl md:text-4xl">
-          Sobre AstracodeLAB
-        </h2>
+      <section id="about" className="mx-30 my-20 md:mx-100 md:my-52">
+        <div className="relative z-0 md:w-3/4 ">
+          <h2 className="relative z-10 inline-block bg-gradient-to-r from-accent via-lime-200 to-white font-sans font-medium text-xl md:text-4xl px-2 rounded">
+            Sobre AstracodeLAB
+          </h2>
+        </div>
         <div className="flex flex-col items-center">
           <img
-            className="rounded-full my-6 shadow-xl size-60"
+            className="rounded-full my-6 shadow-xl w-60 h-60"
             src={about}
             alt="Tres programadoras con fondo espacial"
           />
-          <article className="font-semibold font-sans text-sm border-2 border-black rounded-2xl px-4 py-2 my-2 mx-6 shadow-xl">
+          <article className="font-semibold font-sans text-sm border-2 border-black rounded-2xl p-4 my-2 mx-6 shadow-xl">
             <p className="my-3">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -23,7 +24,7 @@ export const AboutUs = () => {
             </p>
             <p className="my-3">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
           </article>
         </div>
