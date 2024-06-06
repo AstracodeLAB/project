11ca8fcdc00b6705b40b071db5752f1e3ffb5d56
Lourdesjupo <lourdes.jupo@gmail.com>
2024-06-06 12:44:25 +0200

    Add privacy policy, without personal data

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,7 +1,242 @@
-function PrivacyPolicy () {
-  return (<>
-  🎉Privacy
-  </>
-  )
+function PrivacyPolicy() {
+  return (
+    <>
+      <section className='m-10'>
+        <h1 className='text-xl pb-8'>Política de privacidad</h1>
+        <p>
+          Este texto legal le da detalles de cómo recopilamos y procesamos sus
+          datos personales a través del uso de nuestro sitio web
+          <span className='bg-red-200'> PONER EL LINK DE LA WEB</span>,
+          incluyendo cualquier información que pueda proporcionarnos a través
+          del sitio cuando proporciona sus datos a través del formulario
+          habilitado para tal fin.
+        </p>
+        <br></br>
+        <p>
+          Al proporcionarnos los datos, le informamos que nuestros servicios no
+          son posibles para aquellas personas que la normativa les impide
+          prestar consentimiento, por lo que cuando nos remite los formularios
+          nos garantiza que tiene la capacidad suficiente para otorgar
+          consentimiento.
+        </p>
+        <br></br>
+        <p>
+          A continuación le informamos sobre la política de protección de datos
+          de: <span className='bg-red-200'> PONER NOMBRE </span>
+        </p>
+        <br></br>
+        <ol>
+          <li>1. Responsable del tratamiento. </li>
+          <p className='bg-red-200'>
+            Datos de contacto del responsable: XXXXX, con CIF: XXXXXX y
+            domicilio en: XXXXX, XXXXX xxxxxx y teléfono: xxxxxx. Email:
+            xxxxxxxx
+          </p>
+          <p className='bg-red-200'>
+            xxxxx, es el responsable de sus datos. (En adelante nosotros o
+            nuestro).
+          </p>
+          <br></br>
+          <li>2. ¿Qué datos recopilamos?</li>
+          <p>
+            El Reglamento General de Protección de Datos, define los datos
+            personales como toda información sobre una persona física
+            identificada o identificable, es decir cualquier información capaz
+            de identificar a una persona. Esto no incluiría los datos anónimos,
+            ni porcentuales. Los datos de carácter personal que se pudieran
+            recabar directamente del interesado, serán tratados de forma
+            confidencial y quedarán incorporados a las correspondientes
+            actividades del tratamiento, titularidad de
+            <span className='bg-red-200'> xxxxxxx.</span>
+          </p>
+          <br></br>
+          <p>
+            En nuestra Web podemos procesar ciertos tipos de datos personales,
+            que podrán incluir:
+            <ul>
+              <li>• Datos de contacto: email y nombre.</li>
+            </ul>
+          </p>
+          <br></br>
+          <li> 3. ¿Cómo recopilamos sus datos personales? </li>
+          <p>
+            A través del formulario de nuestra página web, a través de nuestros
+            email de contacto o por teléfono, cuando:
+            <br></br>• Solicita información de nuestros servicios
+          </p>
+          <br></br>
+          <p>
+            Para asegurar la calidad de nuestro portal, tenemos que reservamos
+            el derecho a rechazar cualquier solicitud de registro o bien de
+            suspender o cancelar un registro previamente aceptado si entendemos
+            que no cumple estos requisitos o cualquier otra ley o norma. Si esto
+            ocurre, intentaremos exponer los motivos de nuestra decisión, pero
+            no nos podemos comprometer a hacerlo en todos los casos.
+          </p>
+          <li> 4. Finalidad y legitimidad para el uso de sus datos.</li>
+          <p>
+            Los usos más comunes de sus datos personales son:
+            <ol>
+              <li className='bg-red-200'>
+                - Para la formalización de un contrato entre XXXXXX y usted.
+              </li>
+              <li>
+                - Cuando preste su consentimiento en el tratamiento de sus
+                datos.
+              </li>
+              <li>
+                - Cuando los necesitemos para dar cumplimiento a una obligación
+                legal o regulatoria.
+              </li>
+              <li>
+                - Cuando sea necesario para nuestro interés legítimo o de un
+                tercero.
+              </li>
+            </ol>
+          </p>
+        
+        <br />
+        <p className='bg-red-200'>
+          El Usuario podrá revocar el consentimiento prestado en cualquier
+          momento enviando un correo a XXXXXXXXXXX consultando el apartado de
+          ejercicio de derechos más adelante.
+        </p>
+        <br />
+        <p className='bg-red-200'>
+          A continuación adjuntamos una tabla en la que pueden consultar las
+          formas en las que vamos a utilizar sus datos personales y la
+          legitimidad para su uso, además de saber qué tipo de datos personales
+          vamos a tratar. Podemos procesar algún dato personal por algún motivo
+          legal adicional, por lo que si necesita detalles al respecto puede
+          enviar un correo electrónico a  xxxxxxponer mailxxxxxxxx
+          Formulario Finalidad Tipo de datos Legitimidad para su tratamiento
+          Contacto o concertar una cita La finalidad es la gestión de contactos
+          y solicitudes de información recibidas vía web - Email Consentimiento
+          del interesado (art. 6.1.a RGPD)
+        </p>
+        <br />
+        <p>
+          Medidas precontractuales (art. 6.1.b RGPD) Tratamiento es necesario
+          para la satisfacción de intereses legítimos perseguidos por el
+          responsable del tratamiento (art. 6.1f RGPD)
+        </p>
+        <br />
+        <p>
+          Finalidad: sólo utilizaremos sus datos para los fines para los que los
+          recopilamos, a menos que consideremos razonablemente que debemos
+          usarlo para otro motivo, notificándoselo previamente para que esté
+          informado del motivo legal de su procesamiento y siempre y cuando el
+          fin sea compatible con el propósito original.
+        </p>
+          <br />
+        <li> 5. ¿Durante cuánto tiempo conservaremos sus datos?</li> 
+          <p>Se conservarán durante el tiempo
+          necesario para cumplir con la finalidad para la que se recabaron y
+          para determinar las posibles responsabilidades que se pudieran derivar
+          de dicha finalidad y del tratamiento de los datos. Será de aplicación
+          lo dispuesto en las diferentes normativas respecto al plazo de
+          conservación, en lo que resulte de aplicación al presente tratamiento.
+          Datos de los suscriptores por e-mail o formulario: Desde que el
+          usuario se suscribe hasta que se dé de baja.</p>
+          <br />
+           <li>6. Menores de edad. </li>
+          <p className='bg-red-200'>
+            XXXXXXXXXXXnombre XXXXXX no autoriza a los/las menores de 14 años a
+            facilitar sus datos personales a través de los medios habilitados en
+            este sitio web (cumplimentación de los formularios web para la
+            solicitud de servicios, de contacto o mediante el envío de correos
+            electrónicos). Por tanto, las personas que faciliten datos personales
+            utilizando dichos medios manifiestan formalmente ser mayores de 14
+            años quedando XXXXXXXXXXXXXXX  eximido de cualquier
+            responsabilidad por el incumplimiento de este requisito. Si su hijo/a
+            de edad menor al límite de edad establecido, ha proporcionado
+            información personal a XXXXXXXXXXXXXXXXXX, póngase en contacto
+            para poder solicitar el ejercicio de sus derechos aplicables. En
+            aquellos casos en los que los servicios ofrecidos por XXXXXXXXXXXXXX estén destinados a menores de 14 años se habilitarán los
+            medios para recabar la autorización de los padres o tutores legales
+            del/de la menor
+          </p> 
+          <br />
+          <li>7. Ejercicio de los Derechos de Protección de Datos:</li>
+          <p className='bg-red-200'>
+            ¿Cómo ejercer dichos derechos? Los usuarios podrán dirigir una
+            comunicación al domicilio social de XXXXX nombreXXXXXX o
+            dirección de correo electrónico info@clinicadentalanoreta.com,
+            incluyendo en ambos casos fotocopia de su D.N.I u otro documento de
+            identificación similar, para solicitar el ejercicio de los siguientes
+            derechos: - Acceso a sus datos personales: podrá preguntar a Ana
+            Isabel Casero Reina si está usando sus datos personales. - A solicitar
+            su rectificación, si no fuesen correctos, o a ejercer el derecho al
+            olvido con respecto a los mismos. - A solicitar la limitación del
+            tratamiento, en este caso, solo serán conservados por Ana Isabel
+            Casero Reina para el ejercicio o defensa de reclamaciones - A oponerse
+            a su tratamiento: XXXXX nombreXXXXXX dejará tratar los datos en
+            la forma que nos indiques, salvo que por motivos legítimos o para el
+            ejercicio o defensa de posibles reclamaciones, estos deban seguir
+            tratándose. - A la portabilidad de los datos: en caso de que quieras
+            que sus datos sean tratados por otra firma, XXXXX nombreXXXXXX,
+            le facilitará la portabilidad de sus datos al nuevo responsable. Podrá
+            utilizar los modelos puestos a su disposición por la Agencia Española
+            de Protección de Datos, para ejercer sus anteriores derechos: Aquí
+            Reclamar ante la AEPD: si considera que hay un problema con la forma
+            en que XXXXX nombreXXXXXX está tratando sus datos, podrá dirigir
+            sus reclamaciones a la autoridad de control correspondiente, siendo en
+            España, la competente para ello: Agencia Española de Protección de
+            Datos. Le solicitaremos información específica para ayudarnos a
+            confirmar su identidad y garantizar su derecho a acceder a sus datos
+            personales (o ejercer cualquier otro de los derechos mencionados
+            anteriormente). Esto es una medida de seguridad para garantizar que
+            los datos personales no se revelen a ninguna persona que no tenga
+            derecho a recibirlos. Todas las solicitudes las solventaremos en el
+            plazo legal indicado de un mes. No obstante, puede llevarnos más de un
+            mes si su solicitud es particularmente compleja. En este caso, le
+            notificaremos y le mantendremos actualizado.
+          </p>
+           <li>8. Comunicación de datos:</li>
+          <p >
+            prestación de servicios. Es posible que, en el desempeño de nuestro
+            trabajo, necesitemos de la ayuda de terceros, que solo trataran los
+            datos para prestar el servicio contratado, y con los que disponemos de
+            las correspondientes medidas para garantizar sus derechos: </p>
+            <ol>
+               <li>              
+            -Proveedores de servicios que brindan servicios de administración de
+            sistemas y tecnología de la información.</li>
+            <li>
+              - Asesores profesionales que
+              incluyen abogados, auditores y aseguradores que brindan servicios de
+              consultoría bancarios, legales, de seguros y de contabilidad.
+            </li>
+
+            </ol>
+            <p className='bg-red-200'>
+              Todos los
+              encargados de tratamiento a quienes transferimos sus datos respetarán
+              la seguridad de sus datos personales y los tratarán de acuerdo al
+              RGPD. Solo permitimos que dichos encargados traten sus datos para
+              fines determinados y de conformidad con nuestras instrucciones. No
+              obstante puede solicitarnos, en cumplimiento de la transparencia un
+              listado de quienes son estas empresas que nos prestan servicios, puede
+              hacerlo al email: XXXXXX email
+            </p>
+           <li>9. Seguridad de Datos.</li>
+          <p>
+            Hemos implementado las medidas de seguridad adecuadas para evitar que
+            sus datos personales se pierdan accidentalmente, se usen o accedan de
+            forma no autorizada, se modifiquen o divulguen. Además, limitamos el
+            acceso a sus datos personales a aquellos empleados, agentes
+            contratistas y otros terceros que tengan una necesidad comercial de
+            conocer dichos datos. Solo procesarán sus datos personales según
+            nuestras instrucciones y estarán sujetos a un deber de
+            confidencialidad. Hemos implementado procedimientos para tratar
+            cualquier sospecha de violación de sus datos personales y se lo
+            notificaremos a usted y a la Autoridad de Control en caso de que
+            ocurriera, tal como queda regulado en el RGPD en sus artículos 33 y
+            34, una brecha de seguridad.
+          </p>        
+        </ol>
+      </section>
+    </>
+  );
 }
-export default PrivacyPolicy 
+export default PrivacyPolicy;
