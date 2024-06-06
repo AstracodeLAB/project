3385ce905c2c8a1c74ca2d25d9e1946cc998ce9a
AstracodeLAB <astracodelab@gmail.com>
2024-06-06 11:16:40 +0200

    Add grid function

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import painting from "../assets/painting.jpg";
 
-function ProjectsGrid() {
+const projects = [
+  {
+    title: "Proyecto 1",
+    description: "Descripción del proyecto",
+    image: painting,
+  },
+  {
+    title: "Proyecto 2",
+    description: "Descripción del proyecto",
+    image: painting,
+  },
+  {
+    title: "Proyecto 3",
+    description: "Descripción del proyecto",
+    image: painting,
+  },
+
+  // Aquí se van agregando más proyectos
+];
+
+const ProjectsGrid = () => {
   return (
-    <div>ProjectsGrid</div>
-  )
-}
+    <div>
+      {projects.map((projects, index) => (
+        <div
+          key={index}
+          className="bg-black rounded-lg overflow-hidden shadow-lg"
+        >
+          <img
+            src={projects.image}
+            alt={projects.title}
+            className="w-full h-64 object-cover rounded-t-lg"
+          />
+          <div className="p-4 bg-black text-white font-sans rounded-b-lg">
+            <h2 className="text-xl font-bold">{projects.title}</h2>
+            <p className="mt-2">{projects.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default ProjectsGrid
+export default ProjectsGrid;
