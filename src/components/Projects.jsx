24075aa59882da50036c0ea48466f004667dab29
AstracodeLAB <astracodelab@gmail.com>
2024-06-06 11:04:10 +0200

    Fix projects section title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,12 +3,13 @@ import Carousel from "./Carousel";
 
 const Projects = () => {
   return (
-    <section id="projects" className="mx-30 my-15">
-      <div className="absolute inset-6 z-0 h-4 bg-gradient-to-r from-lime-500 via-lime-200 to-white"></div>
-      <h2 className="relative z-10 my-4 font-sans font-medium text-2xl">
-        Proyectos
-      </h2>
-      <div>
+    <section id="projects" className="mx-30 my-25 md:mx-100 md:my-52">
+      <div className="relative z-0 md:w-3/4 ">
+        <h2 className="relative z-10 bg-gradient-to-r from-accent via-lime-200 to-white font-sans font-medium text-xl px-2 rounded md:inline-block md:text-2xl">
+          Proyectos
+        </h2>
+      </div>
+      <div className="my-30">
         <Carousel />
       </div>
     </section>
