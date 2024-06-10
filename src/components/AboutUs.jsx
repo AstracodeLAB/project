671e6fd2f3e8cdb64e9841ed12160c26960fb004
AstracodeLAB <astracodelab@gmail.com>
2024-06-10 16:33:13 +0200

    Remove React imports

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import about from "../assets/about.png";
 
 export const AboutUs = () => {
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import projects from "../data/projectsData";
 
 const Carousel = () => {
