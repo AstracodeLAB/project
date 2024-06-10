2be7302a4fb14b1d215f05324c9fd8eafe1e7fc4
AstracodeLAB <astracodelab@gmail.com>
2024-06-10 16:59:03 +0200

    Fix Hero.jsx and package.json

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between  mt-4 mx-7">
+    <div className="flex items-center justify-between mt-4 mx-7">
       <a href="/">
         <img className="w-20" src={Logo} alt="logo" />
       </a>
@@ -38,7 +38,7 @@ function Header() {
                 <line x1="6" y1="6" x2="18" y2="18" />
               </svg>
             </div>
-            <ul className="flex flex-col items-center justify-between min-h-[250px] text-xl font-semibold text-secondary ">
+            <ul className="flex flex-col items-center justify-between min-h-[250px] text-xl font-semibold text-secondary">
               <li className="border-b border-accent my-8 uppercase">
                 <a href="/about">Servicios</a>
               </li>
@@ -46,7 +46,7 @@ function Header() {
                 <a href="/portfolio">Sobre AstracodeLAB</a>
               </li>
               <li className="border-b border-accent my-8 uppercase">
-                <a href="/contact">Proyectos</a>
+                <a href="/projects">Proyectos</a>
               </li>
               <li className="border-b border-accent my-8 uppercase">
                 <a href="/contact">Contacto</a>
@@ -54,43 +54,41 @@ function Header() {
             </ul>
           </div>
         </section>
-        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex text-accent text-xl font-semibold">
-          <li>
+
         <ul className="DESKTOP-MENU hidden space-x-16 lg:flex text-accent text-xl font-normal lg:text-2xl text-shadow-md">
-          <li > 
+          <li>
             <a href="/about">Servicios</a>
           </li>
           <li>
             <a href="/portfolio">Sobre AstracodeLAB</a>
           </li>
           <li>
-            <a href="/contact">Proyectos</a>
+            <a href="/projects">Proyectos</a>
           </li>
-          <li className="text-accent bg-primary border border-secondary pr-2 pl-2">
-          <li className='text-accent bg-primary border border-secondary pr-2 pl-2 rounded-lg hover:bg-accent hover:text-primary shadow-shadowWhite'>
+          <li className="text-accent bg-primary border border-secondary pr-2 pl-2 rounded-lg hover:bg-accent hover:text-primary shadow-shadowWhite">
             <a href="/contact">CONTACTAR</a>
           </li>
         </ul>
       </nav>
       <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: #1B1D34;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
+        .hideMenuNav {
+          display: none;
+        }
+        .showMenuNav {
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 100vh;
+          top: 0;
+          left: 0;
+          background: #1B1D34;
+          z-index: 10;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          align-items: center;
+        }
+      `}</style>
     </div>
   );
 }
