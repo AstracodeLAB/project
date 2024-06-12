69be84c2aed17e2c1cc6a1138ae7c96a428b19e3
AstracodeLAB <astracodelab@gmail.com>
2024-06-12 13:03:43 +0200

    Add cursor:pointer to hamburger menu. Add rotation effect when hover to hamburger menu. Add changing color hover to X

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,77 +1,77 @@
 import Logo from '../assets/prueba1Logo.png';
-import { useState } from "react";
+import { useState } from 'react';
 
-function Header () {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+function Header() {
+	const [isNavOpen, setIsNavOpen] = useState(false);
 
-  return (
-    <div className="flex items-center justify-between  mt-4 ml-7 mr-7">
-      <a href="/">
-        <img className="w-20" src={Logo} alt="logo" />
-      </a>
-      <nav>
-        <section className="MOBILE-MENU flex lg:hidden">
-          <div
-            className="HAMBURGER-ICON space-y-2"
-            onClick={() => setIsNavOpen((prev) => !prev)}
-          >
-          <span className="block h-1.5 w-9 animate-pulse bg-accent rounded-full"></span>
-          <span className="block h-1.5 w-9 animate-pulse bg-accent rounded-full"></span>
-          <span className="block h-1.5 w-9 animate-pulse bg-accent rounded-full"></span>
-          </div>
+	return (
+		<div className='flex items-center justify-between  mt-4 ml-7 mr-7'>
+			<a href='/'>
+				<img className='w-20' src={Logo} alt='logo' />
+			</a>
+			<nav>
+				<section className='MOBILE-MENU flex lg:hidden cursor-pointer'>
+					<div
+						className='HAMBURGER-ICON space-y-2 transform transition-transform duration-300 hover:rotate-90'
+						onClick={() => setIsNavOpen((prev) => !prev)}
+					>
+						<span className='block h-1.5 w-9 animate-pulse bg-accent rounded-full transition-transform duration-300'></span>
+						<span className='block h-1.5 w-9 animate-pulse bg-accent rounded-full transition-transform duration-300'></span>
+						<span className='block h-1.5 w-9 animate-pulse bg-accent rounded-full transition-transform duration-300'></span>
+					</div>
 
-          <div className={`${isNavOpen ? "showMenuNav" : "hideMenuNav"}`}
->
-            <div
-              className="absolute top-0 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)}
-            >
-              <svg
-                className="h-8 w-8 text-secondary"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
-            </div>
-            <ul className="flex flex-col items-center justify-between min-h-[250px] text-xl font-semibold text-secondary">
-              <li className="border-b border-accent my-8 uppercase">
-                <a href="/about">Servicios</a>
-              </li>
-              <li className="border-b border-accent my-8 uppercase">
-                <a href="/portfolio">Sobre AstracodeLAB</a>
-              </li>
-              <li className="border-b border-accent my-8 uppercase">
-                <a href="/projects">Proyectos</a>
-              </li>
-              <li className="border-b border-accent my-8 uppercase">
-                <a href="/contact">Contacto</a>
-              </li>
-            </ul>
-          </div>
-        </section>
+					<div className={`${isNavOpen ? 'showMenuNav' : 'hideMenuNav'}`}>
+						<div
+							className='absolute top-0 right-0 px-8 py-8 transition-colors duration-300 hover:text-accent'
+							onClick={() => setIsNavOpen(false)}
+						>
+							<svg
+								className='h-8 w-8 text-secondary transition-colors duration-300 hover:text-accent'
+								viewBox='0 0 24 24'
+								fill='none'
+								stroke='currentColor'
+								strokeWidth='2'
+								strokeLinecap='round'
+								strokeLinejoin='round'
+							>
+								<line x1='18' y1='6' x2='6' y2='18' />
+								<line x1='6' y1='6' x2='18' y2='18' />
+							</svg>
+						</div>
+						<ul className='flex flex-col items-center justify-between min-h-[250px] text-xl font-semibold text-secondary'>
+							<li className='border-b border-accent my-8 uppercase'>
+								<a href='/about'>Servicios</a>
+							</li>
+							<li className='border-b border-accent my-8 uppercase'>
+								<a href='/portfolio'>Sobre AstracodeLAB</a>
+							</li>
+							<li className='border-b border-accent my-8 uppercase'>
+								<a href='/projects'>Proyectos</a>
+							</li>
+							<li className='border-b border-accent my-8 uppercase'>
+								<a href='/contact'>Contacto</a>
+							</li>
+						</ul>
+					</div>
+				</section>
 
-        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex text-accent text-xl font-semibold">
-          <li > 
-            <a  href="/about">Servicios</a>
-          </li>
-          <li>
-            <a href="/portfolio">Sobre AstracodeLAB</a>
-          </li>
-          <li>
-            <a href="/projects">Proyectos</a>
-          </li>
-          <li className='text-accent bg-primary border border-secondary pr-2 pl-2'>
-            <a href="/contact">CONTACTAR</a>
-          </li>
-        </ul>
-      </nav>
-      <style>{`
+				<ul className='DESKTOP-MENU hidden space-x-8 lg:flex text-accent text-xl font-semibold'>
+					<li>
+						<a href='/about'>Servicios</a>
+					</li>
+					<li>
+						<a href='/portfolio'>Sobre AstracodeLAB</a>
+					</li>
+					<li>
+						<a href='/projects'>Proyectos</a>
+					</li>
+					<li className='text-accent bg-primary border border-secondary pr-2 pl-2'>
+						<a href='/contact'>CONTACTAR</a>
+					</li>
+				</ul>
+			</nav>
+			<style>{`    
+
         .hideMenuNav {
           display: none;
         }
@@ -89,10 +89,11 @@ function Header () {
           justify-content: space-evenly;
           align-items: center;
         }
+
+        
       `}</style>
-    </div>
-  );
+		</div>
+	);
 }
 
-
-  export default Header;
+export default Header;
