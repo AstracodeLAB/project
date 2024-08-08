8defb3fa8d2ff0c953708debbcf3d5100f3e2adc
AstracodeLAB <astracodelab@gmail.com>
2024-08-08 17:28:09 +0200

    Add i18n

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,58 +1,55 @@
-import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
-import services from '../data/servicesData';
+import { useTranslation } from 'react-i18next';
+import { servicesData } from '../data/servicesData';
 
 export const Services = () => {
-const {t} = useTranslation();
-
 	const [activeServiceIndex, setActiveServiceIndex] = useState(null);
+	const { t } = useTranslation();
 
 	const handleToggle = (serviceIndex) => {
 		setActiveServiceIndex(activeServiceIndex === serviceIndex ? null : serviceIndex);
 	};
 
 	return (
-		<>
-			<section className='m-5 mt-mSections md:mx-16 lg:mx-28'>
-				<div className='flex flex-col items-center '>
-					<div className='w-full'>
-						<h2 className='relative z-10 bg-gradient-to-r from-accent via-lime-200 to-transparent font-sans font-medium text-3xl px-2 mb-5 rounded md:inline-block md:text-4xl'>
-							{t('servicesSection.title')}
-						</h2>
-					</div>
-					<ul className='flex flex-col justify-center gap-4 min-w-60 p-3 my-2 lg:grid lg:grid-cols-2 lg:gap-12'>
-						{services.map((service, serviceIndex) => (
-							<li
-								key={serviceIndex}
-								className={`${service.bgClass} rounded-[15px] p-5 lg:p-7 flex flex-col items-center text-center shadow-lg`}
-							>
-								<h3 className='text-xl font-medium flex justify-between items-center lg:text-2xl'>{service.title}</h3>
-								<button className='mt-4 lg:hidden' onClick={() => handleToggle(serviceIndex)}>
-									<svg
-										xmlns='http://www.w3.org/2000/svg'
-										className={`h-6 w-6 transform transition-transform ${
-											activeServiceIndex === serviceIndex ? 'rotate-180' : ''
-										}`}
-										fill='none'
-										viewBox='0 0 24 24'
-										stroke='currentColor'
-									>
-										<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M19 9l-7 7-7-7' />
-									</svg>
-								</button>
-								<p
-									className={`mt-4 text-m lg:text-xl transition-all duration-500 ease-in-out ${
-										activeServiceIndex === serviceIndex ? 'block' : 'hidden'
-									} lg:block `}
-								>
-									{service.description}
-								</p>
-							</li>
-						))}
-					</ul>
+		<section className='m-5 mt-mSections md:mx-16 lg:mx-28'>
+			<div className='flex flex-col items-center'>
+				<div className='w-full'>
+					<h2 className='relative z-10 bg-gradient-to-r from-accent via-lime-200 to-transparent font-sans font-medium text-3xl px-2 mb-5 rounded md:inline-block md:text-4xl'>
+						{t('services.title', 'Servicios')}
+					</h2>
 				</div>
-			</section>
-		</>
+				<ul className='flex flex-col justify-center gap-4 min-w-60 p-3 my-2 lg:grid lg:grid-cols-2 lg:gap-12'>
+					{servicesData.map((service, serviceIndex) => (
+						<li
+							key={serviceIndex}
+							className={`${service.bgClass} rounded-[15px] p-5 lg:p-7 flex flex-col items-center text-center shadow-lg`}
+						>
+							<h3 className='text-xl font-medium flex justify-between items-center lg:text-2xl'>{t(service.title)}</h3>
+							<button className='mt-4 lg:hidden' onClick={() => handleToggle(serviceIndex)}>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									className={`h-6 w-6 transform transition-transform ${
+										activeServiceIndex === serviceIndex ? 'rotate-180' : ''
+									}`}
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke='currentColor'
+								>
+									<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M19 9l-7 7-7-7' />
+								</svg>
+							</button>
+							<p
+								className={`mt-4 text-m lg:text-xl transition-all duration-500 ease-in-out ${
+									activeServiceIndex === serviceIndex ? 'block' : 'hidden'
+								} lg:block`}
+							>
+								{t(service.description)}
+							</p>
+						</li>
+					))}
+				</ul>
+			</div>
+		</section>
 	);
 };
 
--- src/data/servicesData.jsx
+++ src/data/servicesData.jsx
@@ -1,25 +1,22 @@
-const services = [
+export const servicesData = [
 	{
-		title: 'Desarrollo de aplicaciones web personalizadas',
-		description: 'Creamos aplicaciones web a medida que se adaptan a tus necesidades y objetivos.',
+		title: 'services.webDevelopment.title',
+		description: 'services.webDevelopment.description',
 		bgClass: 'bg-bgBlack text-secondary',
 	},
 	{
-		title: 'Diseño y Experiencia de Usuario (UX/UI)',
-		description: 'Diseñamos interfaces atractivas y fáciles de usar, asegurando una mejor experiencia de usuario.',
+		title: 'services.uxUiDesign.title',
+		description: 'services.uxUiDesign.description',
 		bgClass: 'bg-secondary text-terciary border-4 border-bgBlack',
 	},
 	{
-		title: 'Consultoría Tecnológica',
-		description: 'Ofrecemos asesoramiento para ayudarte a encontrar la mejor solución para tu web.',
+		title: 'services.consulting.title',
+		description: 'services.consulting.description',
 		bgClass: 'bg-bgBlack text-secondary',
 	},
 	{
-		title: 'Mantenimiento y Soporte Continuo',
-		description:
-			'Proporcionamos servicios de mantenimiento y soporte para asegurar que tu sitio web siempre esté en funcionamiento y actualizado.',
+		title: 'services.maintenance.title',
+		description: 'services.maintenance.description',
 		bgClass: 'bg-secondary text-terciary border-4 border-bgBlack',
 	},
 ];
-
-export default services;
