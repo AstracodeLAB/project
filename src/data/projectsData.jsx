aeeba6d5e10bd04933f31e0e37eae384fee093ea
AstracodeLAB <astracodelab@gmail.com>
2024-08-12 14:11:19 +0200

    Add link to restaurant project

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,28 +1,31 @@
 //import painting from "../assets/painting.jpg";
-import escritora from "../assets/Escritora.jpg";
-import maquillaje from "../assets/maquillaje.png";
-import restaurante from "../assets/restaurante.jpg";
+import escritora from '../assets/Escritora.jpg';
+import maquillaje from '../assets/maquillaje.png';
+import restaurante from '../assets/restaurante.jpg';
 
 const projects = [
-  {
-    title: "Blog/Porfolio escritora",
-    description: "Web sencilla, plantillas predefinidas, perfecta para mostrar tus obras, conseguir feedback de la comunidad y promocionar tu trabajo",
-    image: escritora,
-    url: "http://project10102837.tilda.ws/",
-  },
-  {
-    title: "Blog/Porfolio maquilladora",
-    description: "Web diseño libre, nos adaptamos a tus necesidades para que puedas mostrar tu trabajo tal y como lo habías imaginado",
-    image: maquillaje,
-    url: "https://jazzy-sawine-757ded.netlify.app",
-  },
-  {
-    title: "Web restaurante",
-    description: "En esta web se pueden aplicar funciones de reserva, correo incluso gestión de los datos desde el cliente",
-    image: restaurante,
-    url: "https://link-al-proyecto-3.com",
-  },
-  // Aquí se van agregando más proyectos
+	{
+		title: 'Blog/Porfolio escritora',
+		description:
+			'Web sencilla, plantillas predefinidas, perfecta para mostrar tus obras, conseguir feedback de la comunidad y promocionar tu trabajo',
+		image: escritora,
+		url: 'http://project10102837.tilda.ws/',
+	},
+	{
+		title: 'Blog/Porfolio maquilladora',
+		description:
+			'Web diseño libre, nos adaptamos a tus necesidades para que puedas mostrar tu trabajo tal y como lo habías imaginado',
+		image: maquillaje,
+		url: 'https://jazzy-sawine-757ded.netlify.app',
+	},
+	{
+		title: 'Web restaurante',
+		description:
+			'En esta web se pueden aplicar funciones de reserva, correo incluso gestión de los datos desde el cliente',
+		image: restaurante,
+		url: 'https://restaurant-frontend-gemy.onrender.com/',
+	},
+	// Aquí se van agregando más proyectos
 ];
 
 export default projects;
